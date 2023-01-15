a4d5c304e92df0b0b7f77a3555eba5bbec362ad8
Robert Pickering <robert@strangelights.com>
2023-01-15 09:10:55 +0100

    native interop example

---
+++ Unit4.2/NativeInteropSimpleExample/Program.fs
@@ -0,0 +1,28 @@
+﻿namespace InteropWithNative
+open System.Runtime.InteropServices
+
+module Windows =
+    [<DllImport("Kernel32.dll")>]
+    extern int GetCurrentProcessId()
+
+module Unix =
+    [<DllImport("libc")>]
+    extern int getpid()
+
+module Program =
+
+    [<EntryPoint>]
+    let main(args) =
+
+        let isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+
+        let pid =
+                if isWindows then Windows.GetCurrentProcessId()
+                else Unix.getpid()
+
+        let platform = if isWindows then "" else "not "
+
+        printfn $"My platform is {platform}Windows"
+        printfn $"My pid is {pid}"
+
+        0
