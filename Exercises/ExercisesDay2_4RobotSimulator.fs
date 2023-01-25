0739826ef39398436eaf18a1f2026b7b9afa668f
PaulNowak36 <paul.nowak@algosup.com>
2023-01-25 20:02:16 +0100

    Started robot simulator

--- Exercises/ExercisesDay2_4RobotSimulator.fs
+++ Exercises/ExercisesDay2_4RobotSimulator.fs
@@ -35,112 +35,127 @@ module ExercisesDay2_4RobotSimulator =
     type Position = int * int
     type Robot = { direction: Direction; position: Position }
 
-    let create direction position = __
+    let create direction position = 
+        let newRobot = {direction = direction; position = position}
+        newRobot
 
-    let move instructions robot = __
+    let move instructions robot =
+        let robotDirection = robot.direction
+        let robotRight robot =
+            match robotDirection with
+            |Direction.West -> Direction.North
+            |Direction.North -> Direction.East
+            |Direction.East -> Direction.South
+            |Direction.South -> Direction.West
+             
+            {robot with direction = robotDirection}
+            
+
+        if instructions = "R" then robotRight robot
+        else robot 
 
 
-    [<Ignore("Not implemented");Test>]
+    [<Test>]
     let ``RobotSimulator - RobotSimulator - At origin facing north`` () =
         let expected = create Direction.North (0, 0)
         create Direction.North (0, 0) |> AssertEquality expected
 
-    [<Ignore("Not implemented");Test>]
+    [<Test>]
     let ``RobotSimulator - At negative position facing south`` () =
         let expected = create Direction.South (-1, -1)
         create Direction.South (-1, -1) |> AssertEquality expected
 
-    [<Ignore("Not implemented");Test>]
+    [<Test>]
     let ``RobotSimulator - Changes north to east`` () =
         let robot = create Direction.North (0, 0)
         let expected = create Direction.East (0, 0)
         move "R" robot |> AssertEquality expected
 
-    [<Ignore("Not implemented");Test>]
+    [<Test>]
     let ``RobotSimulator - Changes east to south`` () =
         let robot = create Direction.East (0, 0)
         let expected = create Direction.South (0, 0)
         move "R" robot |> AssertEquality expected
 
-    [<Ignore("Not implemented");Test>]
+    [<Test>]
     let ``RobotSimulator - Changes south to west`` () =
         let robot = create Direction.South (0, 0)
         let expected = create Direction.West (0, 0)
         move "R" robot |> AssertEquality expected
 
-    [<Ignore("Not implemented");Test>]
+    [<Test>]
     let ``RobotSimulator - Changes west to north`` () =
         let robot = create Direction.West (0, 0)
         let expected = create Direction.North (0, 0)
         move "R" robot |> AssertEquality expected
 
-    [<Ignore("Not implemented");Test>]
+    [<Test>]
     let ``RobotSimulator - Changes north to west`` () =
         let robot = create Direction.North (0, 0)
         let expected = create Direction.West (0, 0)
         move "L" robot |> AssertEquality expected
 
-    [<Ignore("Not implemented");Test>]
+    [<Test>]
     let ``RobotSimulator - Changes west to south`` () =
         let robot = create Direction.West (0, 0)
         let expected = create Direction.South (0, 0)
         move "L" robot |> AssertEquality expected
 
-    [<Ignore("Not implemented");Test>]
+    [<Test>]
     let ``RobotSimulator - Changes south to east`` () =
         let robot = create Direction.South (0, 0)
         let expected = create Direction.East (0, 0)
         move "L" robot |> AssertEquality expected
 
-    [<Ignore("Not implemented");Test>]
+    [<Test>]
     let ``RobotSimulator - Changes east to north`` () =
         let robot = create Direction.East (0, 0)
         let expected = create Direction.North (0, 0)
         move "L" robot |> AssertEquality expected
 
-    [<Ignore("Not implemented");Test>]
+    [<Test>]
     let ``RobotSimulator - Facing north increments Y`` () =
         let robot = create Direction.North (0, 0)
         let expected = create Direction.North (0, 1)
         move "A" robot |> AssertEquality expected
 
-    [<Ignore("Not implemented");Test>]
+    [<Test>]
     let ``RobotSimulator - Facing south decrements Y`` () =
         let robot = create Direction.South (0, 0)
         let expected = create Direction.South (0, -1)
         move "A" robot |> AssertEquality expected
 
-    [<Ignore("Not implemented");Test>]
+    [<Test>]
     let ``RobotSimulator - Facing east increments X`` () =
         let robot = create Direction.East (0, 0)
         let expected = create Direction.East (1, 0)
         move "A" robot |> AssertEquality expected
 
-    [<Ignore("Not implemented");Test>]
+    [<Test>]
     let ``RobotSimulator - Facing west decrements X`` () =
         let robot = create Direction.West (0, 0)
         let expected = create Direction.West (-1, 0)
         move "A" robot |> AssertEquality expected
 
-    [<Ignore("Not implemented");Test>]
+    [<Test>]
     let ``RobotSimulator - Moving east and north from README`` () =
         let robot = create Direction.North (7, 3)
         let expected = create Direction.West (9, 4)
         move "RAALAL" robot |> AssertEquality expected
 
-    [<Ignore("Not implemented");Test>]
+    [<Test>]
     let ``RobotSimulator - Moving west and north`` () =
         let robot = create Direction.North (0, 0)
         let expected = create Direction.West (-4, 1)
         move "LAAARALA" robot |> AssertEquality expected
 
-    [<Ignore("Not implemented");Test>]
+    [<Test>]
     let ``RobotSimulator - Moving west and south`` () =
         let robot = create Direction.East (2, -7)
         let expected = create Direction.South (-3, -8)
         move "RRAAAAALA" robot |> AssertEquality expected
 
-    [<Ignore("Not implemented");Test>]
+    [<Test>]
     let ``RobotSimulator - Moving east and north`` () =
         let robot = create Direction.South (8, 4)
         let expected = create Direction.North (11, 5)
